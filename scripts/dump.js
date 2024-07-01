7cef4da534fd4062a08b3e59af0a9f999a0b4dfa
Daniel D’Aquino <daniel@daquino.me>
2024-07-01 13:32:56 -0700

    Add UUID map to the dump script
    This is needed to aid customer support requests where locating users'
    UUIDs is relevant
    
    Signed-off-by: Daniel D’Aquino <daniel@daquino.me>
    Reviewed-by: William Casarin <jb55@jb55.com>

--- scripts/dump.js
+++ scripts/dump.js
@@ -7,16 +7,20 @@ function go() {
 	const db = lmdb.open({ path: db_path })
 	const accounts = db.openDB('accounts')
 	const pubkeys_to_user_ids = db.openDB('pubkeys_to_user_ids')
+	const pubkeys_to_user_uuids = db.openDB('pubkeys_to_user_uuids')
 	const invoices = db.openDB('invoices')
 	const sessions = db.openDB('checkout_sessions')
 	const data = {}
-	const datas = {accounts:{},pubkeys_to_user_ids:{},checkout_sessions:{}}
+	const datas = {accounts:{},pubkeys_to_user_ids:{},checkout_sessions:{}, pubkeys_to_user_uuids:{}}
 
 	for (const { key, value } of accounts.getRange()) {
 		datas.accounts[key] = value
 	}
 	for (const { key, value } of pubkeys_to_user_ids.getRange()) {
 		datas.pubkeys_to_user_ids[key] = value
+	}
+	for(const { key, value } of pubkeys_to_user_uuids.getRange()) {
+		datas.pubkeys_to_user_uuids[key] = value
 	}
 	for (const { key, value } of sessions.getRange()) {
 		datas.checkout_sessions[key] = value
